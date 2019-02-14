25cdd39b4c3da90faee072169e8fa6bd3e7b83eb
Jorge Aguilar <cokeaguilar29@gmail.com>
2019-02-14 18:16:19 +0100

    Juego con HUD y sin quitar la vida en numeros ni las balas

---
+++ Assets/RecogerMunicion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecogerMunicion : MonoBehaviour {
+
+    public Slider municion;
+
+	// Use this for initialization
+	void Start () {
+        int parsed = int.Parse(municionDisponible.balas.ToString());
+        municion.maxValue = parsed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int parsed = int.Parse(municionDisponible.balas.ToString());
+        municion.value = parsed;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube")
+        {
+            int maximum = municionDisponible.balas += 150;
+            municion.maxValue = maximum;
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/saltar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saltar : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float fuerza = 5f;
+    Vector3 salta;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        salta = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       /* if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(salta * fuerza, ForceMode.Impulse);
+            //Debug.Log("Salta");
+        }*/
+
+    }
+}
